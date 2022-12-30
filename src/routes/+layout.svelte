5a6606dde6919b14054e5f9b36938074be37abfb
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-12-30 17:19:36 -0300

    feat: create pages layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
   import "../app.css"
 </script>
 
+<nav class="flex gap-8 bg-gray-800 p-4">
+  <span class="text-left text-xl font-semibold tracking-tight text-white">SvelteKit Movie DB</span>
+
+  <div class="flex gap-4 text-sm ">
+    <a href="/" class="nav-button">Home</a>
+    <a href="/movies" class="nav-button">Movies</a>
+  </div>
+</nav>
+
 <slot />
