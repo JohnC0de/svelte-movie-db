696a871ac3e6510fb692caa974be2d087e6d6fea
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-12-30 17:20:43 -0300

    feat: add zod  and refactor home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
-<script lang="ts">
-  import type { PageServerLoad } from "./$types"
-  export let data: PageServerLoad
-</script>
-
-<div>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <pre class="mt-4 text-sm text-gray-500">{JSON.stringify(data, null, 2)}</pre>
+<div class="mt-8 flex flex-col items-center gap-4">
+  <h1 class="text-4xl font-semibold  tracking-tight">SvelteKit Movie DB</h1>
+  <p class="text-xl font-semibold tracking-tight ">A simple movie database</p>
+  <a href="/movies">
+    <button class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"> View Movies </button>
+  </a>
 </div>
